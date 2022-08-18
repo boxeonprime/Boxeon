3df7cb88bfb49d28732c8cd478a901fb9e4bf5f7
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-08-18 14:54:50 -0400

    mvp

--- laravel/resources/views/errors/500.blade.php
+++ laravel/resources/views/errors/500.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.index')
 @section('title', 'Boxeon.com 500 Error')
 @section('content')
-<section class='section alert-info  rounded-corners margin-top-4-em maxw1035'>
+<section class='section alert-info rounded-corners margin-top-4-em maxw1035'>
     <h1 class="extra-large-font primary-olor">Internal Server Error</h1>
 <h1 class='primary-color'>Oops! Something went wrong on our end. Please try again.</h1>
 </section>
