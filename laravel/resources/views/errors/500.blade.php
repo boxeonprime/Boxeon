019bf75ac2006f7180291d09be92a5e42eaf3576
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-08-18 14:48:11 -0400

    MVP

--- laravel/resources/views/errors/500.blade.php
+++ laravel/resources/views/errors/500.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.index')
-@section('title', 'Boxeon | 500 Error')
+@section('title', 'Boxeon.com 500 Error')
 @section('content')
-<section class='section'>
-<img id='image-error-page' class='center display-block' src='../assets/images/general-error.svg' alt='Maintenance workers'/>
-<h1 class='centered primary-color'>Oops! Something went wrong on our end. Please try again.</h1>
+<section class='section alert-info  rounded-corners margin-top-4-em maxw1035'>
+    <h1 class="extra-large-font primary-olor">Internal Server Error</h1>
+<h1 class='primary-color'>Oops! Something went wrong on our end. Please try again.</h1>
 </section>
 </div>
 </div>
