df20d7c94bfdfa27ab29f481d6eae1f29abacff9
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-09-17 10:40:47 -0400

    MVP

--- public_html/assets/js/global.js
+++ public_html/assets/js/global.js
@@ -1278,12 +1278,6 @@ window.onload = function () {
 
   // Close the dropdown menu if the user clicks outside of it
 
-  // Conversions
-  gtag('event', 'conversion', {
-    'send_to': 'AW-1008829526/GDl7CO_9uN8DENaIhuED',
-    'transaction_id': 'sub'
-});
-
 
   window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
