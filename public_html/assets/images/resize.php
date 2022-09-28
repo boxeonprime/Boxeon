69b64f20a7b193a71653ad9bc77c3fa186662fab
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-09-28 17:07:22 -0400

    MVP

--- public_html/assets/images/resize.php
+++ public_html/assets/images/resize.php
@@ -8,11 +8,16 @@ foreach ($files as $file) {
 
     $re = explode(".", $file);
 
-    if($re[1] == "jpeg" || $re[1] == "jpg" || $re[1] == "png"  ){
-       unlink($file);
-    }
+    //if($re[1] == "jpeg" || $re[1] == "jpg" || $re[1] == "png"  ){
+       //unlink($file);
+  //  }
   
+  if (exif_imagetype($file) == 2) { //  jpeg
+    $name = $re[0] . ".webp";
+    $image = imagecreatefromjpeg($file);
+    imagewebp($image, $name);
 
+}
 
 
 }
