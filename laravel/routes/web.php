486c044a11c751272d5ee4440cbc3f54acf5495b
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-10-14 05:25:30 -0400

    MVP

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
+Route::get('/command', function () {
+	
+	/* php artisan migrate */
+    Artisan::call('view:clear');
+    dd("Done");
+});
+
 Route::get('/social-media-share', [SocialShareButtonsController::class,'ShareWidget']);
 
  // ON LOCAL ONLY
